abcdd2454d0873732a06d43a4a00b07995e4dc94
Moses361 <odhismoses20@gmail.com>
2024-02-17 13:17:46 +0300

    add concatination logic

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ let num1 = 3;
 let  num2 = 2;
 let shopping =['bread', 'sugar', 'milk', ];
 let solution = num1 / num2;
+let name = "Moses"
+let newMessage = `${message} my name is ${name}.`;
 console.log(solution);
 console.log(shopping)
+console.log(newMessage);
